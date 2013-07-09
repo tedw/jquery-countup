736dc794245261773a8ec12c37d143fa65d4838d
Ted Whitehead <ted.whitehead@threespot.com>
2013-07-09 15:20:07 -0400

    Add files

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-jquery-countup
-==============
+#jQuery CountUp
+Animates counting up from 1 to a given number
 
-jQuery plugin that animates counting up to a given number
+##Example usage
+
+    <p id="counter">50</p>
+
+    $('#counter').countUp({
+    	duration: 5000,
+    	easing: 'linear'
+    });
---
+++ jquery-countup.js
@@ -0,0 +1,48 @@
+/* CountUp jQuery Plugin */
+// Inspired by http://stackoverflow.com/questions/2540277/jquery-counter-to-count-up-to-a-target-number
+// Using jQuery plugin tempate from https://github.com/jquery-boilerplate/boilerplate/
+
+;(function ( $, window, document, undefined ) {
+
+		var pluginName = "countUp",
+			defaults = {
+				duration: 1500,
+				easing: 'swing'
+			};
+
+		function Plugin ( element, options ) {
+				this.element = element;
+				this.settings = $.extend( {}, defaults, options );
+				this._defaults = defaults;
+				this._name = pluginName;
+				this.init();
+		}
+
+		Plugin.prototype = {
+			init: function () {
+				var $el = $(this.element);
+				this.animateCount( $el );
+			},
+			animateCount: function ( $el ) {
+				var num = $el.text();
+				$el.animate({ countNum: num }, {
+					duration: this.settings.duration,
+					easing: this.settings.easing,
+					step: function() {
+						$el.text(Math.ceil( this.countNum ));
+					}
+				});
+			}
+		};
+
+		// A really lightweight plugin wrapper around the constructor,
+		// preventing against multiple instantiations
+		$.fn[ pluginName ] = function ( options ) {
+			return this.each(function() {
+				if ( !$.data( this, "plugin_" + pluginName ) ) {
+					$.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+				}
+			});
+		};
+
+})( jQuery, window, document );
